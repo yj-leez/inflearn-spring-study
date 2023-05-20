405b1be3f88135e8636864441b22b0b915239c83
yoonjoo <yj_leez@naver.com>
2023-05-20 20:22:24 +0900

    first commit

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class OrderItem {
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @Column(name = "ORDER_ID")
+    private Long orderId;
+
+    @Column(name = "ITEM_ID")
+    private Long itemId;
+    private int orderPrice;
+    private int count;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public void setOrderPrice(int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
