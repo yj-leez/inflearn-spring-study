be848356c46512ae46b86c00c8bf6fd776d43776
‘yj-leez’ <yj_leez@naver.com>
2024-05-23 10:29:36 +0900

    db: 스프링과 문제해결 - 트랜잭션

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,54 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 매니저
+ */
+@RequiredArgsConstructor
+@Slf4j
+public class MemberServiceV3_1 {
+
+    private final PlatformTransactionManager transactionManager;
+    private final MemberRepositoryV3 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException, IllegalAccessException {
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            // 비즈니스 로직
+            bizLogic(fromId, toId, money);
+            transactionManager.commit(status); // 트랜잭션 성공 -> 커밋
+        } catch (Exception e) {
+            transactionManager.rollback(status); // 트랜잭션 실패 -> 롤백
+            throw new IllegalAccessException();
+        }
+
+
+
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException, IllegalAccessException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) throws IllegalAccessException {
+        if (toMember.getMemberId().equals("ex")){
+            throw new IllegalAccessException("이체중 예외");
+        }
+    }
+}
